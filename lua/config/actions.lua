16bda3db03677887ae72abee4104a41d161ea755
m1sh4k <mishaivan373@gmail.com>
2025-09-02 07:45:18 +0300

    mvp

--- lua/config/actions.lua
+++ lua/config/actions.lua
@@ -1,5 +1,50 @@
 local M = {}
 
-M['python'] = {'python3 <filename>'}
+CR_key = vim.api.nvim_replace_termcodes("i", true, false, true)
 
+M['python'] = {
+    {'ExecuteCode',
+    function(extension)
+        vim.api.nvim_command('update | split | terminal python3 % ')
+        local key = vim.api.nvim_replace_termcodes("i", true, false, true)
+        vim.api.nvim_feedkeys(key, "n", false)
+    end
+    },
+}
+
+M['c++'] = {
+    {'ExecuteCode',
+    function(extension)
+        local path = vim.api.nvim_buf_get_name(0)
+        local bodyname = path:gsub(".*/", ""):gsub(extension, ""):sub(1, -2)
+        vim.api.nvim_command('update')
+        local compile_log = vim.fn.system('g++ ' .. path .. ' -o ' .. bodyname)
+        local error = vim.v.shell_error
+        if error == 0 then
+            vim.api.nvim_command('split | terminal ./' .. bodyname)
+            vim.api.nvim_feedkeys(CR_key, "n", false)
+        else
+            print(compile_log)
+        end
+    end
+    }
+}
+
+M['c'] = {
+    {'ExecuteCode',
+    function(extension)
+        local path = vim.api.nvim_buf_get_name(0)
+        local bodyname = path:gsub(".*/", ""):gsub(extension, ""):sub(1, -2)
+        vim.api.nvim_command('update')
+        local compile_log = vim.fn.system('gcc ' .. path .. ' -o ' .. bodyname)
+        local error = vim.v.shell_error
+        if error == 0 then
+            vim.api.nvim_command('split | terminal ./' .. bodyname)
+            vim.api.nvim_feedkeys(CR_key, "n", false)
+        else
+            print(compile_log)
+        end
+    end
+    }
+}
 return M
--- lua/file-actions-nvim/init.lua
+++ lua/file-actions-nvim/init.lua
@@ -4,10 +4,20 @@ Filetypes = require('config.filetypes')
 local M = {}
 
 function M.setup()
-    local ft = Filetypes[vim.api.nvim_buf_get_name(0):gsub(".*%.", "")]
-    vim.api.nvim_create_user_command('FileAction', function(opts)
-        
-    end, {})
+    local extension, _ = vim.api.nvim_buf_get_name(0):gsub(".*%.", "")
+    local ft = Filetypes[extension]
+    for ftypes, act in pairs(Actions) do
+        if ft == ftypes then
+--            print(ftypes)
+--            print(act)
+            for _, command in pairs(act) do
+                vim.api.nvim_create_user_command('FileActions' .. command[1], function(opts)
+                command[2](extension)
+                end, {})
+--                print(command)
+            end
+        end
+    end
 end
-
+--M.setup()
 return M
