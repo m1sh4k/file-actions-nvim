213e48efb5cfe647dd5584122269599399fb3a59
m1sh4k <mishaivan373@gmail.com>
2025-10-31 18:56:11 +0300

    fix: filetype changes any time buffer changes

--- lua/file-actions-nvim/init.lua
+++ lua/file-actions-nvim/init.lua
@@ -1,23 +1,10 @@
 Actions = require('config.actions')
 Filetypes = require('config.filetypes')
-
+Handler = require('file-actions-nvim.buffer_change_handler')
 local M = {}
 
 function M.setup()
-    local extension, _ = vim.api.nvim_buf_get_name(0):gsub(".*%.", "")
-    local ft = Filetypes[extension]
-    for ftypes, act in pairs(Actions) do
-        if ft == ftypes then
---            print(ftypes)
---            print(act)
-            for _, command in pairs(act) do
-                vim.api.nvim_create_user_command('FileActions' .. command[1], function(opts)
-                command[2](extension)
-                end, {})
---                print(command)
-            end
-        end
-    end
+
 end
---M.setup()
+
 return M
