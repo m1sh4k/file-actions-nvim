48994a9da3708cfba72b4d8fe9fd8da2de58792e
m1sh4k <mishaivan373@gmail.com>
2025-12-24 12:54:58 +0300

    fix: removed universal commands initialization on buffer change; fix: user command maps to a buffer instead global

--- lua/file-actions-nvim/buffer_change_handler.lua
+++ lua/file-actions-nvim/buffer_change_handler.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
     local extension, _ = vim.api.nvim_buf_get_name(0):gsub(".*%.", "")
     local ft = Filetypes[extension]
     for ftypes, act in pairs(Actions) do
-        if ft == ftypes or ftypes == 'universal' then
+        if ft == ftypes then
             for _, command in pairs(act) do
                 vim.api.nvim_create_user_command('FileActions' .. command[1], function(opts)
                 command[2](extension)
